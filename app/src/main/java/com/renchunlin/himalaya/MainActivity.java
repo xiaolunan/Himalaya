35e4ff8693f5efdf1b909fba5ab709be472d45bd
RCL <18383934095@163.com>
2020-08-31 21:01:42 +0800

    集成了喜马拉雅sdk，并且测试成功.

--- app/src/main/java/com/renchunlin/himalaya/MainActivity.java
+++ app/src/main/java/com/renchunlin/himalaya/MainActivity.java
@@ -3,6 +3,16 @@ package com.renchunlin.himalaya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, String> map = new HashMap<String, String>();
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(CategoryList categoryList) {
+                List<Category> categories = categoryList.getCategories();
+                if (categories != null) {
+                    int size = categories.size();
+                    Log.i("chen", "categories size -----<: " + size);
+                    for (Category category : categories) {
+                        Log.i("chen", "categories--->: " + category.getCategoryName());
+                    }
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.i("chen", "onError:--> " + i + ",error msg-->" + s);
+            }
+        });
     }
 }
